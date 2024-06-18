3c8f9859569de5701a1c1df92baf7595079bee99
xeim <xeimdonot@gmail.com>
2024-06-18 23:37:29 +0300

    fix bug

--- mambo/lib/source/uncompress.cpp
+++ mambo/lib/source/uncompress.cpp
@@ -1,1 +1,8 @@
 #include "uncompress.hpp"
+
+int mambo::uncompress(const char *archive, const char *where) {
+    return 0; // Return 0 if success
+}
+int mambo::uncompress(const std::string &archive, const std::string &where)  {
+    return uncompress(archive.c_str(), where.c_str());
+}
--- mambo/lib/source/uncompress.hpp
+++ mambo/lib/source/uncompress.hpp
@@ -4,12 +4,8 @@
 #include <string>
 
 namespace mambo {
-    int uncompress(const char* archive, const char* where){
-        return 0; // Return 0 if success
-    }
-    int uncompress(const std::string& archive, const std::string& where) {
-        return uncompress(archive.c_str(), where.c_str());
-    }
+    int uncompress(const char* archive, const char* where);
+    int uncompress(const std::string& archive, const std::string& where);
 }
 
 #endif //MAMBO_UNCOMPRESS_HPP
