9c7c1d1faf42ba1b5385920d044c135ab4063ddf
xeim <xeimdonot@gmail.com>
2024-06-20 01:25:04 +0300

    added huffmancodes

--- mambo/lib/source/detail.cpp
+++ mambo/lib/source/detail.cpp
@@ -1,5 +1,9 @@
 #include "detail.hpp"
 
+#include <unordered_map>
+#include <queue>
+#include <iostream>
+
 int mambo::detail::getFileSize(std::fstream &stream){
     const auto prev = stream.tellg();
     stream.seekg(std::ios::end);
@@ -14,4 +18,43 @@ int mambo::detail::getFileSize(const std::string &path) {
     int size = getFileSize(stream);
     stream.close();
     return size;
+}
+
+bool fun(mambo::detail::node* l, mambo::detail::node* r) {
+    return l->frequency > r->frequency;
+}
+
+
+mambo::detail::node* mambo::detail::getHuffmanCodes(std::fstream &stream) {
+    std::unordered_map<char, int> nodes;
+    std::string line;
+    while(std::getline(stream, line)){
+        for(auto c : line) ++nodes[c];
+    }
+
+    if(nodes.empty()) return nullptr;
+
+    std::priority_queue<node*, std::vector<node*>, nodecomparator> queue;
+
+    for(auto pair : nodes) {
+        queue.push(new node{pair.first, pair.second, nullptr, nullptr});
+    }
+
+    while(queue.size()>1){
+        node* temp = new node{'\0', 0, nullptr, nullptr};
+
+        temp->left = queue.top();
+        temp->frequency+=temp->left->frequency;
+        queue.pop();
+
+        if(!queue.empty()){
+            temp->right = queue.top();
+            temp->frequency+=temp->right->frequency;
+            queue.pop();
+        }
+
+        queue.push(temp);
+    }
+
+    return queue.top();
 }
--- mambo/lib/source/detail.hpp
+++ mambo/lib/source/detail.hpp
@@ -5,8 +5,25 @@
 #include <string>
 
 namespace mambo::detail {
+    // Struct which stay first in .mam
+    struct header {
+        long size;
+        long characterTable;
+        // Table..
+    };
+    struct node {
+        char symbol;
+        int frequency;
+        node* left = nullptr;
+        node* right = nullptr;
+    };
+    struct nodecomparator {
+        bool operator()(node*l, node*r) { return l->frequency>r->frequency; }
+    };
+
     int getFileSize(std::fstream& stream);
     int getFileSize(const std::string& path);
+    node* getHuffmanCodes(std::fstream& stream);
 }
 
 #endif //MAMBO_DETAIL_HPP
