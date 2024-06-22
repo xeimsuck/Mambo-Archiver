c948ec3e69494f2ff8326491bab9b4cd7b87542a
xeim <xeimdonot@gmail.com>
2024-06-22 21:47:26 +0300

    fix bug

--- mambo/lib/source/detail.cpp
+++ mambo/lib/source/detail.cpp
@@ -101,7 +101,7 @@ void mambo::detail::getBits(mambo::detail::node* root, std::vector<int>&v, std::
     v.pop_back();
 }
 
-std::string mambo::detail::writeBits(const std::string &file, const std::unordered_map<char, std::vector<int>>& map) {
+std::string mambo::detail::writeBits(const std::string &file, std::unordered_map<char, std::vector<int>>& map) {
     std::string result;
 
     std::fstream stream (file, std::ios::in | std::ios::binary);
