fa3bef8d10f407a1001b333fc8089f7918064e0a
xeim <xeimdonot@gmail.com>
2024-06-28 21:49:56 +0000

    added uncompress function realization

--- mambo/lib/source/uncompress.cpp
+++ mambo/lib/source/uncompress.cpp
@@ -1,8 +1,37 @@
 #include "uncompress.hpp"
 
+#include <fstream>
+#include "detail/detail.hpp"
+#include <iostream>
+
 int mambo::uncompress(const char *archive, const char *where) {
-    return 0; // Return 0 if success
+    return mambo::uncompress(static_cast<std::string>(archive), static_cast<std::string>(where));
 }
 int mambo::uncompress(const std::string &archive, const std::string &where)  {
-    return uncompress(archive.c_str(), where.c_str());
+    std::fstream in(archive, std::ios::in | std::ios::binary);
+    if(!in.is_open()) return 1;
+
+    if(!detail::checkSignature(in)){
+        in.close();
+        return 1;
+    }
+
+    auto huffmanMap = detail::readHuffmanMap(in);
+    if(huffmanMap.empty()){
+        in.close();
+        return 1;
+    }
+
+    std::size_t filesCount = 0;
+    in.read(reinterpret_cast<char*>(&filesCount), sizeof(filesCount));
+    for(int i = 0; i < filesCount; ++i){
+        std::fstream out(where+detail::readFileName(in), std::ios::out);
+        if(!out.is_open()) return 1;
+        out << detail::readCompressedFile(in, huffmanMap);
+        out.close();
+    }
+
+    in.close();
+
+    return 0; // Success
 }
