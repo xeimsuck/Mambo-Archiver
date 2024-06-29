32ea7b042f6aec8445b02edde5b14daed142b604
xeim <xeimdonot@gmail.com>
2024-06-29 14:43:30 +0000

    fix bug

--- mambo/lib/source/uncompress.cpp
+++ mambo/lib/source/uncompress.cpp
@@ -8,6 +8,9 @@ int mambo::uncompress(const char *archive, const char *where) {
     return mambo::uncompress(static_cast<std::string>(archive), static_cast<std::string>(where));
 }
 int mambo::uncompress(const std::string &archive, const std::string &where)  {
+    std::string outDirectory = where;
+    if(outDirectory.back()!='/') outDirectory+='/';
+
     std::fstream in(archive, std::ios::in | std::ios::binary);
     if(!in.is_open()) return 1;
 
@@ -24,8 +27,9 @@ int mambo::uncompress(const std::string &archive, const std::string &where)  {
 
     std::size_t filesCount = 0;
     in.read(reinterpret_cast<char*>(&filesCount), sizeof(filesCount));
+
     for(int i = 0; i < filesCount; ++i){
-        std::fstream out(where+detail::readFileName(in), std::ios::out);
+        std::fstream out(outDirectory+detail::readFileName(in), std::ios::out);
         if(!out.is_open()) return -1;
         out << detail::readCompressedFile(in, huffmanMap);
         out.close();
