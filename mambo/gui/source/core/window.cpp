6cc7563f598683f41d2890d6178cb2f2b6c0e3b4
xeim <xeimdonot@gmail.com>
2024-06-30 13:53:33 +0000

    GUI mode

--- mambo/gui/source/core/window.cpp
+++ mambo/gui/source/core/window.cpp
@@ -1,5 +1,58 @@
 #include "window.hpp"
 
+#include <QFileDialog>
+#include <QDebug>
+#include "mambo.hpp"
+
 mambo::gui::window::window() {
+    using std::make_unique;
+
+    centralWidget = make_unique<QWidget>();
+    this->setCentralWidget(centralWidget.get());
+
+    mainLayout = make_unique<QGridLayout>();
+    centralWidget->setLayout(mainLayout.get());
+
+    stateLabel = make_unique<QLabel>("-");
+    stateLabel->setAlignment(Qt::AlignmentFlag::AlignCenter);
+    stateLabel->setFont(QFont("Arial", 12, QFont::Bold));
+    mainLayout->addWidget(stateLabel.get(), 0, 0);
+
+    buttonsLayout = make_unique<QHBoxLayout>();
+    mainLayout->addLayout(buttonsLayout.get(), 1, 0);
+
+    compressButton = make_unique<QPushButton>("Compress");
+    buttonsLayout->addWidget(compressButton.get());
+    connect(compressButton.get(), &QPushButton::clicked, this, &window::doCompress);
+
+    uncompressButton = make_unique<QPushButton>("Uncompress", this);
+    buttonsLayout->addWidget(uncompressButton.get());
+    connect(uncompressButton.get(), &QPushButton::clicked, this, &window::doUncompress);
+
     this->show();
+}
+
+void mambo::gui::window::doCompress() {
+    auto archive = QFileDialog::getSaveFileName();
+    auto files = QFileDialog::getOpenFileNames();
+    std::vector<std::string> vectorOfFiles(files.size());
+    for(int i = 0; i < files.size(); ++i)
+        vectorOfFiles[i] = files.at(i).toStdString();
+    auto result = mambo::compress(archive.toStdString(), vectorOfFiles);
+    if(result<0){
+        stateLabel->setText("COMPRESS ERROR");
+    } else {
+        stateLabel->setText("COMPRESS RATIO: "+QString::number(result));
+    }
+}
+
+void mambo::gui::window::doUncompress() {
+    auto archive = QFileDialog::getOpenFileName();
+    auto where = QFileDialog::getExistingDirectory();
+    auto result = mambo::uncompress(archive.toStdString(), where.toStdString());
+    if(result){
+        stateLabel->setText("UNCOMPRESS ERROR");
+    } else {
+        stateLabel->setText("UNCOMPRESS SUCCESSFUL");
+    }
 }
--- mambo/gui/source/core/window.hpp
+++ mambo/gui/source/core/window.hpp
@@ -2,11 +2,29 @@
 #define MAMBO_GUI_WINDOW_HPP
 
 #include <QMainWindow>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QGridLayout>
+#include <QLabel>
+#include <memory>
 
 namespace mambo::gui {
     class window final : public QMainWindow{
+        Q_OBJECT
     public:
         window();
+
+    public slots:
+        void doCompress();
+        void doUncompress();
+
+    private:
+        std::unique_ptr<QWidget> centralWidget = nullptr;
+        std::unique_ptr<QGridLayout> mainLayout = nullptr;
+        std::unique_ptr<QLabel> stateLabel = nullptr;
+        std::unique_ptr<QHBoxLayout> buttonsLayout = nullptr;
+        std::unique_ptr<QPushButton> compressButton = nullptr;
+        std::unique_ptr<QPushButton> uncompressButton = nullptr;
     };
 }
 
